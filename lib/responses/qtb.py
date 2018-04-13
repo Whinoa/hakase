3ce939e28da0974f2c0007ec53ed6abdc08723db
ReactorofR <reactorofr@gmail.com>
2018-04-13 21:24:57 +0200

    started on qtb

--- lib/responses/qtb.py
+++ lib/responses/qtb.py
@@ -1,5 +1,27 @@
 import asyncio
 import discord
+import datetime
+
+from lib.client import client
+from lib.models import QtAnimeGirl
+from hakase import config
+
+# Channels where qtb is in progress
+
+
+async def get_new_girls(message):
+  anime_girl = QtAnimeGirl()
+  new_girls = anime_girl.get_new_girls(config['image_directory'])
+  await client.send_message(message.channel, "{} new girls added!".format(new_girls))
+
+async def qtb(message, params):
+  caller = discord.User()
+  girls = [QtAnimeGirl(), QtAnimeGirl()]
+  vote = discord.Message(reactions=[])
+  votes = [0, 0]
+  voters = {}
+  is_ongoing = True
+  time_start = datetime.datetime.now()
 
 # QTB
 # 1. Send start message
